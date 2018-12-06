7f65f61d6f84c5578f225ae1f7e1857a01609a98
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-12-06 18:04:47 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      text: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <form onSubmit={this.handleSubmit}>
+          <textarea
+            id="new-post"
+            placeholder="Type a post"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button class="btn">Save Post</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ""
+    }));
+  }
+}
+
+class List extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.items.map(item => (
+          <div key={item.id}>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
