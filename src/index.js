36021c7c6d6a2f74b128351d2985a18d9ecd80f3
Zixuan Wang <33987311+zixuan75@users.noreply.github.com>
2018-12-31 11:28:20 -0500

    reinstalling RX (part 1)

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import "./styles.css";
 // Properties
 var properties = {
   myProperty: "This is a property",
-  rxdetail: "RX is installed.",
-  rxinstalled: "yes",
+  rxdetail: "RX is not installed.",
+  rxinstalled: "no",
   rxcheck() {
     if (this.rxinstalled === "yes") {
       return (
